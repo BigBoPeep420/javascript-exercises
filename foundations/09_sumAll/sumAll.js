e27239f2e81222e49ce16de8b0ea3cc6ef3fa3e2
BigBoPeep420 <236509553+BigBoPeep420@users.noreply.github.com>
2026-01-04 17:36:37 -0600

    Finished foundations/09-11

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(i1, i2) {
+    if((Number.isInteger(i1) && i1 >= 0) && (Number.isInteger(i2) && i2 >= 0)){
+    let min = Math.min(i1, i2)
+    let max = Math.max(i1, i2)
+    console.log(min + " " + max)
+    let sum = 0
+    for(let i = min; i <= max; i++){
+        sum = sum + i;
+    }
+    console.log(sum);
+    return sum;
+    }
+    else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(i) {
+  c = (i-32) * (5/9);
+  if(Number.isInteger(c)){
+    return c;
+  }else{
+    return +c.toFixed(1);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(i) {
+  f = (i*(9/5)) + 32
+  if(Number.isInteger(f)){
+    return f;
+  }else{
+    return +f.toFixed(1);
+  }
 };
 
 // Do not edit below this line
