12f8145920c76d331e783c6196d20535b1fdedbb
BigBoPeep420 <236509553+BigBoPeep420@users.noreply.github.com>
2026-01-04 14:54:28 -0600

    Finished 01-06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(strToRpeat, repeatNum) {
+    if(repeatNum < 0){
+        return "ERROR";
+    } else {
+        let finalStr = "";
+        for(let i = 0; i < repeatNum; i++){
+            finalStr = finalStr + strToRpeat
+        }
+        console.log(finalStr);
+        return finalStr;
+    }
 };
 
 // Do not edit below this line
