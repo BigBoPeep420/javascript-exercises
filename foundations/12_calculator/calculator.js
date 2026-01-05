7ab28afb0eb0876ca63ef58da1a47185e0b9bc7d
BigBoPeep420 <236509553+BigBoPeep420@users.noreply.github.com>
2026-01-04 18:01:01 -0600

    Finished foundations/12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	fn = 0;
+  for (const num of nums) {
+    fn += num;
+  }
+  return fn;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  fn = 1;
+  for (const num of nums) {
+    fn *= num;
+  }
+  return fn;
 };
 
-const power = function() {
-	
+const power = function(b, e) {
+	return Math.pow(b, e);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  fn = 1;
+	for(let i = 1; i <= num; i++){
+    fn *= i;
+  }
+  return fn;
 };
 
 // Do not edit below this line
