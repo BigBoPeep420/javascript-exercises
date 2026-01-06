e84fe4b95baae874216691c5d30bd121aa58701d
BigBoPeep420 <236509553+BigBoPeep420@users.noreply.github.com>
2026-01-06 16:47:54 -0600

    Finished 15_getTheTitles & 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((accum, elem) => {
+        if(getAge(elem) > getAge(accum)){
+            return elem;
+        }else{
+            return accum;
+        }
+    }, {yearOfBirth: 3000});
+};
 
+function getAge(person){
+    if("yearOfDeath" in person){
+        return person.yearOfDeath - person.yearOfBirth;
+    }else{
+        year = new Date().getFullYear();
+        return year - person.yearOfBirth;
+    }
 };
 
 // Do not edit below this line
