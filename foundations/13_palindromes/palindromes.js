0114fc7cf3a359a864476965b70092db37ce42ce
BigBoPeep420 <236509553+BigBoPeep420@users.noreply.github.com>
2026-01-06 15:18:31 -0600

    Finished 13_palindromes, technically
    Technically failed the last test because my function
        didn't care about numbers, by design.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let str = Array.from(s.toLowerCase()).filter(char => /^[a-zA-Z]$/.test(char));
+    let rev = str.toReversed();
+    return str.join("").localeCompare(rev.join("")) == 0;
 };
 
 // Do not edit below this line
