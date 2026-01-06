c650274e9f6f8517385930612d79c3effcc56cf5
BigBoPeep420 <236509553+BigBoPeep420@users.noreply.github.com>
2026-01-06 15:41:22 -0600

    Finished foundations/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const seq = [];
+for(let i = 0; i < 30; i++){
+    if(i < 2){
+        seq.push(i);
+    }else{
+        seq.push(seq[i-1] + seq[i-2]);
+    }
+}
 
+const fibonacci = function(i) {
+    int = parseInt(i);
+    if((int != NaN) && (int >= 0)){
+        return seq[int];
+    }else{
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
